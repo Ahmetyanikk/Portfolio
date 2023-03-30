ac37da70b0bbd2994bcfe1f6a28b58c85b6de3ca
Ahmet Yanık <ahmetyaniklar@gmail.com>
2023-03-30 13:58:47 +0300

    Mock Final

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
           maxWidth="1024px"
           height={16}
         >
-          <p>Pete • © 2022</p>
+          <p>Ahmet Yanık • © 2023</p>
         </Flex>
       </footer>
     </Box>
